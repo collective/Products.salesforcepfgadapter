08f1a29cb96a962c72f237f11b8261a4185eb66a
Andy Leeb <andy@andersonleeb.com>
2009-01-21 18:04:22 +0000

    flushing out functional tests to serve as basis data grid UI issues as well as presentation of the adapter to various permissioned users

--- Products/salesforcepfgadapter/tests/base.py
+++ Products/salesforcepfgadapter/tests/base.py
@@ -1,6 +1,9 @@
 # Import the base test case classes
 from Testing import ZopeTestCase as ztc
-from Products.CMFPlone.tests import PloneTestCase
+from Products.CMFPlone.tests import PloneTestCase as ptc
+from Products.CMFCore.utils import getToolByName
+
+from Products.salesforcebaseconnector.tests import sfconfig   # get login/pw
 
 # These must install cleanly, ZopeTestCase will take care of the others
 ztc.installProduct('PloneFormGen')
@@ -13,13 +16,25 @@ ztc.installProduct('salesforcepfgadapter')
 # are all products available to Zope in the test fixture)
 PRODUCTS = ['salesforcepfgadapter']
 
-PloneTestCase.setupPloneSite(products=PRODUCTS)
+ptc.setupPloneSite(products=PRODUCTS)
 
-class SalesforcePFGAdapterTestCase(PloneTestCase.PloneTestCase):
+class SalesforcePFGAdapterTestCase(ptc.PloneTestCase):
     """Base class for integration tests for the 'salesforcepfgadapter' product. This may
     provide specific set-up and tear-down operations, or provide convenience
     methods.
     """
+
+class BaseSalesforcePFGAdapterFunctionalTestCase(ptc.FunctionalTestCase):
+    """Base class for functional doctests
+    """
+    def afterSetUp(self):
+        self.portal.manage_addProduct['salesforcebaseconnector'].manage_addTool('Salesforce Base Connector', None)
+        self.salesforce = getToolByName(self.portal, "portal_salesforcebaseconnector")
+        self.salesforce.setCredentials(sfconfig.USERNAME, sfconfig.PASSWORD)
+        self._todelete = list() # keep track of ephemeral test data to delete
+        self.folder.invokeFactory('FormFolder', 'ff1')
+        self.ff1 = getattr(self.folder, 'ff1')
+    
 
 class FakeRequest(dict):
 
