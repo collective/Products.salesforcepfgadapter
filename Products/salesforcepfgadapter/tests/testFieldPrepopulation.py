e313ccbb1de55ff17842e49f4d208742fea7e90d
David Glick <david@glicksoftware.com>
2009-06-26 22:13:26 +0000

    add a few more tests

--- Products/salesforcepfgadapter/tests/testFieldPrepopulation.py
+++ Products/salesforcepfgadapter/tests/testFieldPrepopulation.py
@@ -10,6 +10,7 @@ from zope.event import notify
 from Products.Archetypes.event import ObjectEditedEvent
 
 from Products.salesforcepfgadapter.tests import base
+from Products.salesforcepfgadapter.prepopulator import FieldValueRetriever
 
 class TestFieldPrepopulationSetting(base.SalesforcePFGAdapterTestCase):
     """ test feature that can prepopulate the form from data in Salesforce """
@@ -40,7 +41,6 @@ class TestFieldPrepopulationSetting(base.SalesforcePFGAdapterTestCase):
          - the creation mode is set to update or upsert
          - a primary key field is configured
         """
-        import pdb; pdb.set_trace( )
         self.sfa.setFieldMap(self.test_fieldmap)
         self.sfa.setCreationMode('creation')
         self.sfa.setPrepopulateFieldValues(True)
@@ -57,6 +57,23 @@ class TestFieldPrepopulationSetting(base.SalesforcePFGAdapterTestCase):
         notify(ObjectEditedEvent(self.sfa))
         
         default_expr = self.ff1.replyto.getRawFgTDefault()
+        self.assertEqual(default_expr, 'context/@@sf_value')
+    
+    def testPrepopulateSettingSetsFieldDefaultsForFieldsInFieldsets(self):
+        self.ff1.invokeFactory('FieldsetFolder', 'fieldset')
+        fieldset = self.ff1.fieldset
+        fieldset.invokeFactory('FormStringField', 'foo')
+        fieldset_field = fieldset.foo
+        
+        self.sfa.setSFObjectType('Contact')
+        self.sfa.setFieldMap((dict(field_path= 'fieldset/foo',
+            form_field='', sf_field='Email'),))
+        self.sfa.setCreationMode('upsert')
+        self.sfa.setPrimaryKeyField('ContactId')
+        self.sfa.setPrepopulateFieldValues(True)
+        notify(ObjectEditedEvent(self.sfa))
+
+        default_expr = self.ff1.fieldset.foo.getRawFgTDefault()
         self.assertEqual(default_expr, 'context/@@sf_value')
 
     def testClearingPrepopulateSettingClearsFieldDefaults(self):
@@ -80,17 +97,27 @@ class TestFieldPrepopulationSetting(base.SalesforcePFGAdapterTestCase):
 
         self.assertEqual(self.ff1.replyto.getRawFgTDefault(), 'foobar')
 
-class TestFieldPrepopulation(base.SalesforcePFGAdapterTestCase):
+class TestFieldValueRetriever(base.SalesforcePFGAdapterTestCase):
     """ test feature that can prepopulate the form from data in Salesforce """
 
     def afterSetUp(self):
-        super(TestFieldPrepopulation, self).afterSetUp()
+        super(TestFieldValueRetriever, self).afterSetUp()
         self.folder.invokeFactory('FormFolder', 'ff1')
         self.ff1 = getattr(self.folder, 'ff1')
 
         self.ff1.invokeFactory('SalesforcePFGAdapter', 'salesforce')
         self.sfa = getattr(self.ff1, 'salesforce')
+        self.test_fieldmap = (dict(field_path= 'replyto',
+            form_field='Your E-Mail Address', sf_field='Email'),)
+        self.sfa.setFieldMap(self.test_fieldmap)
+        self.sfa.setCreationMode('upsert')
+        self.sfa.setPrimaryKeyField('Email')
+        self.sfa.setPrepopulateFieldValues(True)
+        notify(ObjectEditedEvent(self.sfa))
+        
+        self._todelete = []
 
+    def _createTestContact(self):
         # create a test contact
         data = dict(type='Contact',
             LastName='Doe',
@@ -101,7 +128,22 @@ class TestFieldPrepopulation(base.SalesforcePFGAdapterTestCase):
             )
         res = self.salesforce.create([data])
         self.objid = id = res[0]['id']
-        self._todelete = [id]
+        self._todelete.append(id)
+    
+    def testGetRelevantSFAdapter(self):
+        retriever = FieldValueRetriever(self.ff1.replyto, self.app.REQUEST)
+        sfa = retriever.getRelevantSFAdapter()
+        self.failUnless(sfa is self.sfa)
+    
+    def testGetRelevantSFAdapterForFieldsetField(self):
+        self.ff1.invokeFactory('FieldsetFolder', 'fieldset')
+        fieldset = self.ff1.fieldset
+        fieldset.invokeFactory('FormStringField', 'foo')
+        fieldset_field = fieldset.foo
+        
+        retriever = FieldValueRetriever(fieldset_field, self.app.REQUEST)
+        sfa = retriever.getRelevantSFAdapter()
+        self.failUnless(sfa is self.sfa)
 
     def beforeTearDown(self):
         """clean up SF data"""
@@ -117,5 +159,5 @@ def test_suite():
     from unittest import TestSuite, makeSuite
     suite = TestSuite()
     suite.addTest(makeSuite(TestFieldPrepopulationSetting))
-    suite.addTest(makeSuite(TestFieldPrepopulation))
+    suite.addTest(makeSuite(TestFieldValueRetriever))
     return suite
