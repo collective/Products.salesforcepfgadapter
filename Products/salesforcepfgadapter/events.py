46add15cae05bc772d2cde2e6572867faa653a5a
Jesse Snyder <jesses@npowerseattle.org>
2009-06-30 04:51:12 +0000

    refactor event handler for apply custom TAL expression for default values

--- Products/salesforcepfgadapter/events.py
+++ Products/salesforcepfgadapter/events.py
@@ -1,25 +1,20 @@
-import logging
 from Acquisition import aq_parent
-from zope.component import adapter, queryUtility
-from zope.app.component.hooks import getSite
-from Products.statusmessages.interfaces import IStatusMessage
-from Products.Archetypes.interfaces import IObjectEditedEvent, IObjectInitializedEvent
+from zope.component import adapter
+from Products.Archetypes.interfaces import IObjectEditedEvent
 
 from Products.salesforcepfgadapter import interfaces
 
-logger = logging.getLogger("Products.salesforcepfgadapter")
-
-DO_UPSERT_FLAG = 'upsert'
-VIEW_NAME = 'context/@@sf_value'
+UPSERT_MODE = 'upsert'
+SF_VIEW = 'context/@@sf_value'
 PFG_EMAIL_DEFAULT = 'here/memberEmail'
 
 def _safe_to_override(field):
     if field.getRawFgTDefault() == PFG_EMAIL_DEFAULT:
         return True
     return not field.getRawFgTDefault() or \
-        field.getRawFgTDefault() == VIEW_NAME
+        field.getRawFgTDefault() == SF_VIEW
 
-def set_default(sf_adapter, new_default):
+def _set_default(sf_adapter, new_default):
     form_folder = aq_parent(sf_adapter)
     sf_key_field = sf_adapter.getPrimaryKeyField()
     mappings = sf_adapter.getFieldMap()
@@ -30,19 +25,21 @@ def set_default(sf_adapter, new_default):
             if _safe_to_override(field):
                 field.setFgTDefault(new_default)
 
+def _sf_defaults_activated(sf_adapter):
+    # if we're upserting and prepopulating, we're active
+    return sf_adapter.getCreationMode() == UPSERT_MODE and \
+                sf_adapter.getPrepopulateFieldValues()
+
+
 @adapter(interfaces.ISalesforcePFGAdapter, IObjectEditedEvent)
 def handle_adapter_saved(sf_adapter, event):
-    logger.info("We hit our handler!")
+    """On save, check if fields should be prepopulated from Salesforce.
+       If so, set the default TAL expression to our custom browser view.
+    """
+    if _sf_defaults_activated(sf_adapter):
+        _set_default(sf_adapter, SF_VIEW)
+        return
     
-    # if we're not upserting, make sure all our defaults are removed
-    if sf_adapter.getCreationMode() != DO_UPSERT_FLAG:
-        set_default(sf_adapter, '')
-        return
-    # if prepopulate is off, make sure all our defaults are removed
-    if not sf_adapter.getPrepopulateFieldValues():
-        set_default(sf_adapter, '')
-        return
-    # otherwise, set default to the browser view 'context/@@sf_value'
-    # for each field that is mapped
-    set_default(sf_adapter, VIEW_NAME)
+    _set_default(sf_adapter, '')
+    return
 
